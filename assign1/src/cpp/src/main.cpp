1fdfd34a0d5ea7a174ba0ede5f2a20d98424b08a
André Júlio Moreira <andre.moreira.9@hotmail.com>
2022-03-16 12:05:14 +0000

    Add c line multiplication

--- assign1/src/cpp/src/main.cpp
+++ assign1/src/cpp/src/main.cpp
@@ -4,13 +4,13 @@
 #include <time.h>
 #include <cstdlib>
 #include <papi.h>
+#include <string.h>
 
 using namespace std;
 
 #define SYSTEMTIME clock_t
 
-void OnMult(int m_ar, int m_br)
-{
+void mult(int m_ar, int m_br) {
 
     SYSTEMTIME Time1, Time2;
 
@@ -18,11 +18,11 @@ void OnMult(int m_ar, int m_br)
     double temp;
     int i, j, k;
 
-    double *pha, *phb, *phc;
+    double* pha, * phb, * phc;
 
-    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
-    phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
-    phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+    pha = (double*)malloc((m_ar * m_ar) * sizeof(double));
+    phb = (double*)malloc((m_ar * m_ar) * sizeof(double));
+    phc = (double*)malloc((m_ar * m_ar) * sizeof(double));
 
     for (i = 0; i < m_ar; i++)
         for (j = 0; j < m_ar; j++)
@@ -34,13 +34,10 @@ void OnMult(int m_ar, int m_br)
 
     Time1 = clock();
 
-    for (i = 0; i < m_ar; i++)
-    {
-        for (j = 0; j < m_br; j++)
-        {
+    for (i = 0; i < m_ar; i++) {
+        for (j = 0; j < m_br; j++) {
             temp = 0;
-            for (k = 0; k < m_ar; k++)
-            {
+            for (k = 0; k < m_ar; k++) {
                 temp += pha[i * m_ar + k] * phb[k * m_br + j];
             }
             phc[i * m_ar + j] = temp;
@@ -53,8 +50,7 @@ void OnMult(int m_ar, int m_br)
 
     // display 10 elements of the result matrix tto verify correctness
     cout << "Result matrix: " << endl;
-    for (i = 0; i < 1; i++)
-    {
+    for (i = 0; i < 1; i++) {
         for (j = 0; j < min(10, m_br); j++)
             cout << phc[j] << " ";
     }
@@ -66,26 +62,67 @@ void OnMult(int m_ar, int m_br)
 }
 
 // add code here for line x line matriz multiplication
-void OnMultLine(int m_ar, int m_br)
-{
+void multLine(int m_ar, int m_br) {
+
+    SYSTEMTIME Time1, Time2;
+
+    char st[100];
+    double temp;
+    int i, j, k;
+
+    double* pha, * phb, * phc;
+
+    pha = (double*)malloc((m_ar * m_ar) * sizeof(double));
+    phb = (double*)malloc((m_ar * m_ar) * sizeof(double));
+    phc = (double*)malloc((m_ar * m_ar) * sizeof(double));
+    memset(phc, 0, (m_ar * m_ar) * sizeof(double));
+
+    for (i = 0; i < m_ar; i++)
+        for (j = 0; j < m_ar; j++)
+            pha[i * m_ar + j] = (double)1.0;
+
+    for (i = 0; i < m_br; i++)
+        for (j = 0; j < m_br; j++)
+            phb[i * m_br + j] = (double)(i + 1);
+
+    Time1 = clock();
+
+    for (i = 0; i < m_ar; i++) {
+        for (k = 0; k < m_br; k++) {
+            for (j = 0; j < m_ar; j++) {
+                phc[i * m_ar + j] += pha[i * m_ar + k] * phb[k * m_br + j]; // pha[i][k] * phb[k][j]
+            }
+        }
+    }
+
+    Time2 = clock();
+    sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+    cout << st;
+
+    // display 10 elements of the result matrix tto verify correctness
+    cout << "Result matrix: " << endl;
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < min(10, m_br); j++)
+            cout << phc[j] << " ";
+    }
+    cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
 }
 
 // add code here for block x block matriz multiplication
-void OnMultBlock(int m_ar, int m_br, int bkSize)
-{
-}
+void multBlock(int m_ar, int m_br, int bkSize) {}
 
-void handle_error(int retval)
-{
+void handle_error(int retval) {
     printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
     exit(1);
 }
 
-void init_papi()
-{
+void init_papi() {
     int retval = PAPI_library_init(PAPI_VER_CURRENT);
-    if (retval != PAPI_VER_CURRENT && retval < 0)
-    {
+    if (retval != PAPI_VER_CURRENT && retval < 0) {
         printf("PAPI library version mismatch!\n");
         exit(1);
     }
@@ -93,12 +130,11 @@ void init_papi()
         handle_error(retval);
 
     std::cout << "PAPI Version Number: MAJOR: " << PAPI_VERSION_MAJOR(retval)
-              << " MINOR: " << PAPI_VERSION_MINOR(retval)
-              << " REVISION: " << PAPI_VERSION_REVISION(retval) << "\n";
+        << " MINOR: " << PAPI_VERSION_MINOR(retval)
+        << " REVISION: " << PAPI_VERSION_REVISION(retval) << "\n";
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
 
     char c;
     int lin, col, blockSize;
@@ -125,10 +161,9 @@ int main(int argc, char *argv[])
         cout << "ERROR: PAPI_L2_DCM" << endl;
 
     op = 1;
-    do
-    {
+    do {
         cout << endl
-             << "1. Multiplication" << endl;
+            << "1. Multiplication" << endl;
         cout << "2. Line Multiplication" << endl;
         cout << "3. Block Multiplication" << endl;
         cout << "Selection?: ";
@@ -144,18 +179,17 @@ int main(int argc, char *argv[])
         if (ret != PAPI_OK)
             cout << "ERROR: Start PAPI" << endl;
 
-        switch (op)
-        {
+        switch (op) {
         case 1:
-            OnMult(lin, col);
+            mult(lin, col);
             break;
         case 2:
-            OnMultLine(lin, col);
+            multLine(lin, col);
             break;
         case 3:
             cout << "Block Size? ";
             cin >> blockSize;
-            OnMultBlock(lin, col, blockSize);
+            multBlock(lin, col, blockSize);
             break;
         }
 
