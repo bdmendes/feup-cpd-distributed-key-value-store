8097b4b33ea8c35571d22976b297349b4af1db75
Bruno Mendes <brod56@sapo.pt>
2022-03-16 11:49:12 +0000

    Add Rust PAPI measures

--- assign1/src/rust/src/main.rs
+++ assign1/src/rust/src/main.rs
@@ -1,28 +1,59 @@
 #![allow(overflowing_literals)]
 
-fn main() {
-    println!("Hello, world!");
+/* PAPI constants */
+const PAPI_OK: i32 = 0;
+const PAPI_L1_DCM: i32 = -2147483648;
+const PAPI_L2_DCM: i32 = -2147483646;
 
+fn operation() {
+    println!("Hello world!");
+}
+
+fn main() {
+    /* Init PAPI */
+    let mut event_set: i32 = papi_sys::PAPI_NULL;
+    let mut values: [i64; 2] = [0, 0];
     unsafe {
-        let mut values: [i64; 1] = [0];
-        let mut test: [i64; 1000000] = [1; 1000000];
-        let retval = papi_sys::PAPI_library_init(papi_sys::PAPI_VER_CURRENT);
-        if retval != papi_sys::PAPI_VER_CURRENT {
-            println!("no");
+        let r = papi_sys::PAPI_library_init(papi_sys::PAPI_VER_CURRENT);
+        if r != papi_sys::PAPI_VER_CURRENT {
+            println!("PAPI library version mismatch!");
             return;
         }
-        let mut event_set = 0;
-
         papi_sys::PAPI_create_eventset(&mut event_set);
-        papi_sys::PAPI_add_event(event_set, 2 | 0x80000000);
+        papi_sys::PAPI_add_event(event_set, PAPI_L1_DCM);
+        papi_sys::PAPI_add_event(event_set, PAPI_L2_DCM);
+    }
 
-        papi_sys::PAPI_start(event_set);
-        
-        for n in 1..1000000 {
-            test[n] = 1 * 3 + 2 / 5;
+    /* Operations */
+    let mut l: bool = true;
+    while l {
+        unsafe {
+            papi_sys::PAPI_start(event_set);
         }
+        operation();
+        l = false;
+        unsafe {
+            if papi_sys::PAPI_stop(event_set, (&mut values) as *mut i64) != PAPI_OK {
+                println!("ERROR: Stop PAPI");
+            }
+            println!("L1 DCM: {}", values[0]);
+            println!("L2 DCM: {}", values[1]);
+            if papi_sys::PAPI_reset(event_set) != PAPI_OK {
+                println!("FAIL reset");
+            }
+        }
+    }
 
-        papi_sys::PAPI_stop(event_set, (&mut values) as *mut i64);
-        println!("L1 DCM: {} \n", values[0]);
+    /* Destroy PAPI */
+    unsafe {
+        if papi_sys::PAPI_remove_event(event_set, PAPI_L1_DCM) != PAPI_OK {
+            println!("FAIL remove event");
+        }
+        if papi_sys::PAPI_remove_event(event_set, PAPI_L2_DCM) != PAPI_OK {
+            println!("FAIL remove event");
+        }
+        if papi_sys::PAPI_destroy_eventset(&mut event_set) != PAPI_OK {
+            println!("FAIL destroy");
+        }
     }
 }
