6f7e68e2e1f224574d85bfc79c14dd87f31c6ac0
Bruno Mendes <brod56@sapo.pt>
2022-03-25 12:05:38 +0000

    Add cpp auto csv generator

--- assign1/cpp/src/main.cpp
+++ assign1/cpp/src/main.cpp
@@ -7,14 +7,11 @@
 #include <cstdlib>
 #include <papi.h>
 #include <string.h>
-
-using namespace std;
-
-#define SYSTEMTIME clock_t
+#include <queue>
 
 void mult(int matrixSize)
 {
-    SYSTEMTIME time1, time2;
+    clock_t time1, time2;
 
     char st[100];
     double temp;
@@ -51,16 +48,13 @@ void mult(int matrixSize)
 
     time2 = clock();
     sprintf(st, "Time: %3.3f seconds\n", (double)(time2 - time1) / CLOCKS_PER_SEC);
-    cout << st;
+    std::cout << st;
 
-    // display 10 elements of the result matrix tto verify correctness
-    cout << "Result matrix: " << endl;
-    for (i = 0; i < 1; i++)
-    {
-        for (j = 0; j < min(10, matrixSize); j++)
-            cout << resultMatrix[j] << " ";
-    }
-    cout << endl;
+    // display 10 elements of the result matrix to verify correctness
+    std::cout << "Result matrix: " << std::endl;
+    for (j = 0; j < std::min(10, matrixSize); j++)
+        std::cout << resultMatrix[j] << " ";
+    std::cout << std::endl;
 
     free(leftMatrix);
     free(rightMatrix);
@@ -69,7 +63,7 @@ void mult(int matrixSize)
 
 void multLine(int matrixSize)
 {
-    SYSTEMTIME time1, time2;
+    clock_t time1, time2;
 
     char st[100];
     double temp;
@@ -105,16 +99,13 @@ void multLine(int matrixSize)
 
     time2 = clock();
     sprintf(st, "Time: %3.3f seconds\n", (double)(time2 - time1) / CLOCKS_PER_SEC);
-    cout << st;
+    std::cout << st;
 
-    // display 10 elements of the result matrix tto verify correctness
-    cout << "Result matrix: " << endl;
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < min(10, matrixSize); j++)
-            cout << resultMatrix[j] << " ";
-    }
-    cout << endl;
+    // display 10 elements of the result matrix to verify correctness
+    std::cout << "Result matrix: " << std::endl;
+    for (j = 0; j < std::min(10, matrixSize); j++)
+        std::cout << resultMatrix[j] << " ";
+    std::cout << std::endl;
 
     free(leftMatrix);
     free(rightMatrix);
@@ -123,7 +114,7 @@ void multLine(int matrixSize)
 
 void multBlock(int matrixSize, int bkSize)
 {
-    SYSTEMTIME time1, time2;
+    clock_t time1, time2;
 
     char st[100];
     double temp;
@@ -152,9 +143,9 @@ void multBlock(int matrixSize, int bkSize)
         {
             for (kBlock = 0; kBlock < matrixSize; kBlock += bkSize)
             {
-                int upperi = min(iBlock + bkSize, matrixSize);
-                int upperj = min(jBlock + bkSize, matrixSize);
-                int upperk = min(kBlock + bkSize, matrixSize);
+                int upperi = std::min(iBlock + bkSize, matrixSize);
+                int upperj = std::min(jBlock + bkSize, matrixSize);
+                int upperk = std::min(kBlock + bkSize, matrixSize);
                 for (i = iBlock; i < upperi; i++)
                 {
                     for (k = kBlock; k < upperk; k++)
@@ -172,173 +163,199 @@ void multBlock(int matrixSize, int bkSize)
 
     time2 = clock();
     sprintf(st, "Time: %3.3f seconds\n", (double)(time2 - time1) / CLOCKS_PER_SEC);
-    cout << st;
+    std::cout << st;
 
-    // display 10 elements of the result matrix tto verify correctness
-    cout << "Result matrix: " << endl;
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < min(10, matrixSize); j++)
-            cout << resultMatrix[j] << " ";
-    }
-    cout << endl;
+    // display 10 elements of the result matrix to verify correctness
+    std::cout << "Result matrix: " << std::endl;
+    for (j = 0; j < std::min(10, matrixSize); j++)
+        std::cout << resultMatrix[j] << " ";
+    std::cout << std::endl;
 
     free(leftMatrix);
     free(rightMatrix);
     free(resultMatrix);
 }
 
-void handle_error(int retval)
+int runTests(int algChoice, int eventSet)
 {
-    printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
-    exit(1);
-}
-
-void init_papi()
-{
-    int retval = PAPI_library_init(PAPI_VER_CURRENT);
-    if (retval != PAPI_VER_CURRENT && retval < 0)
-    {
-        printf("PAPI library version mismatch!\n");
-        exit(1);
-    }
-    if (retval < 0)
-        handle_error(retval);
-
-    std::cout << "PAPI Version Number: MAJOR: " << PAPI_VERSION_MAJOR(retval)
-              << " MINOR: " << PAPI_VERSION_MINOR(retval)
-              << " REVISION: " << PAPI_VERSION_REVISION(retval) << "\n";
-}
-
-int run_tests(int start_size, int max_size, int start_block_size, int max_block, int EventSet) {
-    int matrix_size = start_size;
-    int block_size = start_block_size;
+    std::queue<int> matrixSizes;
+    std::vector<int> blockSizes = {128, 256, 512};
     int ret;
-    long long values[6] = {0,0,0,0,0,0};
-    ofstream multFile("multFile.csv");
-    ofstream lineMultFile("lineMultFile.csv");
-    // ofstream blockMultFile("multFile.csv");
+    long long values[6] = {0, 0, 0, 0, 0, 0};
+    std::string fileName;
 
-    multFile << "SIZE,L1 DCM,L2 DCM,L3 TCM,L3 TCA,L3 PCNTG,TOT INS\n";
-    lineMultFile << "SIZE,L1 DCM,L2 DCM,L3 TCM,L3 TCA,L3 PCNTG,TOT INS\n";
-
-    while(matrix_size <= max_size) {
-        // Start counting
-        ret = PAPI_start(EventSet);
-        if (ret != PAPI_OK)
-            cout << "ERROR: Start PAPI" << endl;
-
-        mult(matrix_size);
-
-        ret = PAPI_stop(EventSet, values);
-        if (ret != PAPI_OK)
-            cout << "ERROR: Stop PAPI" << endl;
-
-        multFile << matrix_size 
-        << ',' << values[0] 
-        << ',' << values[1] 
-        << ',' << values[2] 
-        << ',' << values[3] 
-        << ',' << (double)values[2] / values [3]
-        << ',' << values[4] << '\n';
-
-        ret = PAPI_reset(EventSet);
-        if (ret != PAPI_OK)
-            std::cout << "FAIL reset" << endl;
-
-        // Start counting
-        ret = PAPI_start(EventSet);
-        if (ret != PAPI_OK)
-            cout << "ERROR: Start PAPI" << endl;
-
-        multLine(matrix_size);
-
-        ret = PAPI_stop(EventSet, values);
-        if (ret != PAPI_OK)
-            cout << "ERROR: Stop PAPI" << endl;
-        lineMultFile << matrix_size 
-        << ',' << values[0] 
-        << ',' << values[1] 
-        << ',' << values[2] 
-        << ',' << values[3] 
-        << ',' << (double)values[2] / values [3]
-        << ',' << values[4] << '\n';
-
-        ret = PAPI_reset(EventSet);
-        if (ret != PAPI_OK)
-            std::cout << "FAIL reset" << endl;
-        matrix_size *= 2;
+    int matrixSize = 600;
+    while (matrixSize <= 3000)
+    {
+        matrixSizes.push(matrixSize);
+        matrixSize += 400;
+    }
+    switch (algChoice)
+    {
+    case 1:
+        fileName = "lineMult.csv";
+        matrixSize = 4096;
+        while (matrixSize <= 10240)
+        {
+            matrixSizes.push(matrixSize);
+            matrixSize += 2048;
+        }
+        break;
+    case 2:
+        fileName = "blockMult.csv";
+        matrixSize = 4096;
+        while (matrixSize <= 10240)
+        {
+            matrixSizes.push(matrixSize);
+            matrixSize += 2048;
+        }
+        break;
+    default:
+        fileName = "mult.csv";
     }
 
-    multFile.close();
-    lineMultFile.close();
+    std::ofstream file(fileName);
+
+    if (algChoice == 2)
+    {
+        file << "BLOCK SIZE,";
+    }
+    file << "MATRIX SIZE,L1 DCM,L2 DCM,L3 TCM,L3 TCA,L3 PCNTG,TOT INS,ELAPSED\n";
+
+    while (!matrixSizes.empty())
+    {
+        clock_t time1, time2;
+        double elapsed;
+        matrixSize = matrixSizes.front();
+        matrixSizes.pop();
+
+        for (const auto blockSize : blockSizes)
+        {
+            // Start counting
+            ret = PAPI_start(eventSet);
+            if (ret != PAPI_OK)
+                std::cout << "ERROR: Start PAPI" << std::endl;
+
+            std::cout << std::endl;
+            time1 = clock();
+            switch (algChoice)
+            {
+            case 1:
+                multLine(matrixSize);
+                break;
+            case 2:
+                multBlock(matrixSize, blockSize);
+                break;
+            default:
+                mult(matrixSize);
+                break;
+            }
+            time2 = clock();
+            elapsed = (double)(time2 - time1) / CLOCKS_PER_SEC;
+
+            ret = PAPI_stop(eventSet, values);
+            if (ret != PAPI_OK)
+                std::cout << "ERROR: Stop PAPI" << std::endl;
+
+            if (algChoice == 2)
+            {
+                file << blockSize << ",";
+            }
+
+            file << matrixSize
+                 << ',' << values[0]
+                 << ',' << values[1]
+                 << ',' << values[2]
+                 << ',' << values[3]
+                 << ',' << (double)values[2] / values[3]
+                 << ',' << values[4]
+                 << ',' << elapsed << '\n';
+            file.flush();
+
+            if (algChoice != 2)
+            {
+                break; // no block sizes
+            }
+        }
+    }
+
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
-
     char c = 0;
     int matrixSize = 0, blockSize;
     int op;
 
-    int EventSet = PAPI_NULL;
-    long long values[6] = {0,0,0,0,0,0};
+    int eventSet = PAPI_NULL;
+    long long values[6] = {0, 0, 0, 0, 0, 0};
     int ret;
 
     ret = PAPI_library_init(PAPI_VER_CURRENT);
     if (ret != PAPI_VER_CURRENT)
-        std::cout << "FAIL" << endl;
+        std::cout << "FAIL" << std::endl;
 
-    ret = PAPI_create_eventset(&EventSet);
+    ret = PAPI_create_eventset(&eventSet);
     if (ret != PAPI_OK)
-        cout << "ERROR: create eventset" << PAPI_strerror(ret)  << endl;
+        std::cout << "ERROR: create eventset" << PAPI_strerror(ret) << std::endl;
 
-    ret = PAPI_add_event(EventSet, PAPI_L1_DCM);
+    ret = PAPI_add_event(eventSet, PAPI_L1_DCM);
     if (ret != PAPI_OK)
-        cout << "ERROR: PAPI_L1_DCM" << endl;
+        std::cout << "ERROR: PAPI_L1_DCM" << std::endl;
 
-    ret = PAPI_add_event(EventSet, PAPI_L2_DCM);
+    ret = PAPI_add_event(eventSet, PAPI_L2_DCM);
     if (ret != PAPI_OK)
-        cout << "ERROR: PAPI_L2_DCM " << PAPI_strerror(ret)  << endl;
+        std::cout << "ERROR: PAPI_L2_DCM " << PAPI_strerror(ret) << std::endl;
 
-    ret = PAPI_add_event(EventSet, PAPI_L3_TCM);
+    ret = PAPI_add_event(eventSet, PAPI_L3_TCM);
     if (ret != PAPI_OK)
-        cout << "ERROR: PAPI_L3_TCM"  << PAPI_strerror(ret)  << endl;
+        std::cout << "ERROR: PAPI_L3_TCM" << PAPI_strerror(ret) << std::endl;
 
-    ret = PAPI_add_event(EventSet, PAPI_L3_TCA);
+    ret = PAPI_add_event(eventSet, PAPI_L3_TCA);
     if (ret != PAPI_OK)
-        cout << "ERROR: PAPI_L3_TCA" << PAPI_strerror(ret)  << endl;
+        std::cout << "ERROR: PAPI_L3_TCA" << PAPI_strerror(ret) << std::endl;
 
-    ret = PAPI_add_event(EventSet, PAPI_TOT_INS);
+    ret = PAPI_add_event(eventSet, PAPI_TOT_INS);
     if (ret != PAPI_OK)
-        cout << "ERROR: PAPI_tot" << endl;
+        std::cout << "ERROR: PAPI_tot" << std::endl;
 
     op = 1;
     do
     {
-        cout << endl
-             << "0. Exit" << endl
-             << "1. Multiplication" << endl;
-        cout << "2. Line Multiplication" << endl;
-        cout << "3. Block Multiplication" << endl;
-        cout << "4. Store Values" << endl;
-        cout << "Selection?: ";
-        cin >> op;
-        if (op == 0)
-            break;
+        std::cout << std::endl
+                  << "0. Exit" << std::endl
+                  << "1. Multiplication" << std::endl;
+        std::cout << "2. Line Multiplication" << std::endl;
+        std::cout << "3. Block Multiplication" << std::endl;
+        std::cout << "4. Run tests" << std::endl;
+        std::cout << "Selection?: ";
+        std::cin >> op;
 
-        if(op == 4) {
-            run_tests(512, 1024, 64, 512, EventSet);
+        if (op == 0)
+        {
             break;
         }
+        else if (op == 4)
+        {
+            int algChoice;
+            std::cout << std::endl
+                      << "0. Multiplication" << std::endl;
+            std::cout << "1. Line Multiplication" << std::endl;
+            std::cout << "2. Block Multiplication" << std::endl
+                      << "Selection? ";
+            std::cin >> algChoice;
+            runTests(algChoice, eventSet);
+            break;
+        }
+
         printf("Dimensions: matrixSizes=cols ? ");
-        cin >> matrixSize;
+        std::cin >> matrixSize;
 
         // Start counting
-        ret = PAPI_start(EventSet);
+        ret = PAPI_start(eventSet);
         if (ret != PAPI_OK)
-            cout << "ERROR: Start PAPI" << endl;
+            std::cout << "ERROR: Start PAPI" << std::endl;
 
         switch (op)
         {
@@ -349,92 +366,38 @@ int main(int argc, char *argv[])
             multLine(matrixSize);
             break;
         case 3:
-            cout << "Block Size? ";
-            cin >> blockSize;
+            std::cout << "Block Size? ";
+            std::cin >> blockSize;
             multBlock(matrixSize, blockSize);
+            break;
+        default:
             break;
         }
 
-        ret = PAPI_stop(EventSet, values);
+        ret = PAPI_stop(eventSet, values);
         if (ret != PAPI_OK)
-            cout << "ERROR: Stop PAPI" << endl;
+            std::cout << "ERROR: Stop PAPI" << std::endl;
         printf("L1 DCM: %lld \n", values[0]);
         printf("L2 DCM: %lld \n", values[1]);
         printf("L3 TCM: %lld \n", values[2]);
         printf("L3 TCA: %lld \n", values[3]);
         printf("TOT INS: %lld \n", values[4]);
 
-        ret = PAPI_reset(EventSet);
+        ret = PAPI_reset(eventSet);
         if (ret != PAPI_OK)
-            std::cout << "FAIL reset" << endl;
+            std::cout << "FAIL reset" << std::endl;
 
     } while (op != 0);
 
-    ret = PAPI_remove_event(EventSet, PAPI_L1_DCM);
+    ret = PAPI_remove_event(eventSet, PAPI_L1_DCM);
     if (ret != PAPI_OK)
-        std::cout << "FAIL remove event" << endl;
+        std::cout << "FAIL remove event" << std::endl;
 
-    ret = PAPI_remove_event(EventSet, PAPI_L2_DCM);
+    ret = PAPI_remove_event(eventSet, PAPI_L2_DCM);
     if (ret != PAPI_OK)
-        std::cout << "FAIL remove event" << endl;
+        std::cout << "FAIL remove event" << std::endl;
 
-    ret = PAPI_destroy_eventset(&EventSet);
+    ret = PAPI_destroy_eventset(&eventSet);
     if (ret != PAPI_OK)
-        std::cout << "FAIL destroy" << endl;
+        std::cout << "FAIL destroy" << std::endl;
 }
-
-/**
-
-2 --- 4096
-
-Time: 76.840 seconds
-Result matrix:
-8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06
-L1 DCM: 17725381879
-L2 DCM: 18130499064
-TOT INS: 550024917538
-
-3 --- 4096 x 128
-
-Time: 66.931 seconds
-Result matrix:
-8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06 8.39066e+06
-L1 DCM: 9760764472
-L2 DCM: 30154685498
-TOT INS: 487136182659
-
-
-
-
-
-
-
-2 --- 2048 --
-
-Time: 11.014 seconds
-L1 DCM: 1141916442
-L2 DCM: 2229068626
-L3 TCM: 1456183770
-L3 TCA: 2229148269
-L3 DCA: 2229068626
-
-3 --- 2048 -- 128
-
-Time: 7.815 seconds
-L1 DCM: 1220012714
-L2 DCM: 3561164020
-L3 TCM: 162716262
-L3 TCA: 3561186799
-L3 DCA: 3561164020
-
-3 ---- 2048 -- 256
-
-Time: 6.865 seconds
-L1 DCM: 1138199254
-L2 DCM: 2786496234
-L3 TCM: 44412140
-L3 TCA: 2786519784
-L3 DCA: 2786496234
-
-
-*/
