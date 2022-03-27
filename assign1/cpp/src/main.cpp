48e7639a99d35b27e0cb3ec6032ed64b664ec5ac
Bruno Mendes <brod56@sapo.pt>
2022-03-27 15:53:53 +0100

    Remove miss percentage

--- assign1/cpp/src/main.cpp
+++ assign1/cpp/src/main.cpp
@@ -220,7 +220,7 @@ int runTests(int algChoice, int eventSet)
     {
         file << "BLOCK SIZE,";
     }
-    file << "MATRIX SIZE,L1 DCM,L2 DCM,L3 TCM,L3 TCA,L3 PCNTG,TOT INS,ELAPSED\n";
+    file << "MATRIX SIZE,L1 DCM,L2 DCM,L3 TCM,L3 TCA,TOT INS,ELAPSED\n";
 
     while (!matrixSizes.empty())
     {
@@ -267,7 +267,6 @@ int runTests(int algChoice, int eventSet)
                  << ',' << values[1]
                  << ',' << values[2]
                  << ',' << values[3]
-                 << ',' << (double)values[2] / values[3]
                  << ',' << values[4]
                  << ',' << elapsed << '\n';
             file.flush();
