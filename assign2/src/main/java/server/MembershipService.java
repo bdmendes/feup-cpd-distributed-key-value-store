8e7fa4a6e18c26aae195f668849b4df37f5330cd
André Júlio Moreira <andre.moreira.9@hotmail.com>
2022-05-17 18:18:04 +0100

    Add membership counter and logs

--- assign2/src/main/java/server/MembershipService.java
+++ assign2/src/main/java/server/MembershipService.java
@@ -1,21 +1,88 @@
 package server;
 
 import message.*;
+import utils.StoreUtils;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class MembershipService implements MessageVisitor {
     private final StorageService storageService;
-    private final int membershipCounter = 0;
+    private int nodeMembershipCounter;
+    private final Map<String, Integer> membershipLog;
 
     public MembershipService(StorageService storageService) {
         this.storageService = storageService;
+        this.membershipLog = Collections.synchronizedMap(new LinkedHashMap<>(
+                32, .75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<String, Integer> eldest) {
+                return this.size() > 32;
+            }
+        });
+        this.readMembershipCounterFromFile();
+        this.readMembershipLogFromFile();
     }
 
-    private void sendMembershipStatus() {
+    public Map<String, Integer> getMembershipLog() {
+        return membershipLog;
+    }
+
+    protected void readMembershipCounterFromFile() {
+        try {
+            Scanner scanner = new Scanner(new File(getMembershipCounterFilePath()));
+            nodeMembershipCounter = scanner.nextInt();
+            scanner.close();
+        } catch (Exception e) {
+            nodeMembershipCounter = 0;
+            this.writeMembershipCounterToFile();
+        }
+    }
+
+    protected void writeMembershipCounterToFile() {
+        try {
+            FileWriter fileWriter = new FileWriter(getMembershipCounterFilePath());
+            fileWriter.write(Integer.toString(nodeMembershipCounter));
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected void readMembershipLogFromFile() {
+        try {
+            Scanner scanner = new Scanner(new File(getMembershipLogFilePath()));
+            while (scanner.hasNextLine()){
+                String[] line = scanner.nextLine().split(" ");
+                String nodeId = line[0];
+                int counter = Integer.parseInt(line[1]);
+                membershipLog.put(nodeId, counter);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            this.writeMembershipLogToFile();
+        }
+    }
+
+    protected void writeMembershipLogToFile() {
+        try {
+            FileWriter fileWriter = new FileWriter(getMembershipLogFilePath());
+            this.membershipLog.forEach((key, value) -> {
+                try {
+                    fileWriter.write(key + " " + value);
+                    fileWriter.write(MessageConstants.END_OF_LINE);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void multicastMembershipLog() {
         //
     }
 
@@ -27,8 +94,8 @@ public class MembershipService implements MessageVisitor {
 
     }
 
-    public int getMembershipCounter() {
-        return membershipCounter;
+    public int getNodeMembershipCounter() {
+        return nodeMembershipCounter;
     }
 
     @Override
@@ -91,7 +158,7 @@ public class MembershipService implements MessageVisitor {
         DeleteReply response = new DeleteReply();
         response.setKey(deleteMessage.getKey());
 
-        if(!deleted) {
+        if (!deleted) {
             response.setStatusCode(StatusCode.FILE_NOT_FOUND);
         } else {
             response.setStatusCode(StatusCode.OK);
@@ -147,5 +214,23 @@ public class MembershipService implements MessageVisitor {
     @Override
     public void process(Message message, Socket socket) throws IOException {
         message.accept(this, socket);
+    }
+
+    private String getMembershipCounterFilePath() {
+        return "./node_storage/storage" + storageService.getNode() + "/membership_counter.txt";
+    }
+
+    private String getMembershipLogFilePath() {
+        return "./node_storage/storage" + storageService.getNode() + "/membership_log.txt";
+    }
+
+    protected void addMembershipEvent(String nodeId, int membershipCounter){
+        membershipLog.put(nodeId, membershipCounter);
+        this.writeMembershipLogToFile();
+    }
+
+    protected void incrementCounter() {
+        this.nodeMembershipCounter++;
+        this.writeMembershipCounterToFile();
     }
 }
