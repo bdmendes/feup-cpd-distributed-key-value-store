32281d61a8b8ffeeaf05dee00559d1b12228c27f
Nuno Alves <nyansword@gmail.com>
2022-06-03 00:36:19 +0100

    fix: rollback when it shouldnt

--- assign2/src/main/java/server/state/JoinedNodeState.java
+++ assign2/src/main/java/server/state/JoinedNodeState.java
@@ -347,7 +347,6 @@ public class JoinedNodeState extends NodeState {
             try {
                 this.membershipService.getMulticastHandler().close();
             } catch (IOException e) {
-                this.membershipService.getMembershipCounter().rollbackJoin();
                 e.printStackTrace();
             }
 
